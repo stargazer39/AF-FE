58301f7b00718976198ac1fd2107e1a0bcb0afa6
Semora <senalweerasekara@gmail.com>
2023-05-01 19:18:43 +0530

    changes

--- src/pages/questions/questionModals/addQuestion.jsx
+++ src/pages/questions/questionModals/addQuestion.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function AddQuestionModal({ data }) {
+function AddQuestionModal({ setAddQuestionModal }) {
 
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center">
