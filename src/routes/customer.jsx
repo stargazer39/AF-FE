49b0d105a61bab2059fd8ef45fef54df5d21b122
Semora <senalweerasekara@gmail.com>
2023-05-01 18:15:49 +0530

    adding changes

--- src/routes/customer.jsx
+++ src/routes/customer.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import TestPage from "../pages/Test/test";
 import CreateGroup from "../pages/groups/CreateGroup";
+import Questions from "../pages/questions/questions";
 
 export default function CustomerRouter() {
   return (
     <Routes>
       <Route path="/test" element={<TestPage />} />
       <Route path="/" element={<TestPage />} />
+      <Route path="/questions" element={<Questions />} />
 
       <Route path="/user/addGroup" element={<CreateGroup />} />
     </Routes>
