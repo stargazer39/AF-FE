4261cc926575e88c524b716eb2c891f09ed5954f
Semora <senalweerasekara@gmail.com>
2023-05-04 21:41:42 +0530

    changes

--- src/pages/questions/groupQuestions.jsx
+++ src/pages/questions/groupQuestions.jsx
@@ -28,7 +28,7 @@ function GroupQuestions({groupData, profileID}) {
       .catch(error => {
         console.log(error)
       })
-    }, [update])
+    }, [update, group])
 
     var QuestionData
     var AnswerData
@@ -36,20 +36,25 @@ function GroupQuestions({groupData, profileID}) {
   return (
     // remove this to take it out from center 
     <>
-     <button onClick={()=> setQuesModal(true)} className="px-4 py-2 rounded-md mr-2 text-white bg-gray-400 hover:bg-gray-500 focus:outline-none focus:bg-gray-600">Add Question</button>
-      {questions.map(ques => (
-        <div className="flex flex-col w-5/6">
+    <div className="flex flex-col items-center w-full">
+      <div className=" w-4/6 flex justify-end">
+          <button onClick={()=> setQuesModal(true)} className="mb-8 w-48 px-4 py-2 rounded-md mr-2 text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:bg-blue-700">Add Question</button>
+      </div>
+     
+        {questions.map(ques => (
+          <div className="flex flex-col w-4/6 mb-16">
 
-        <div className="bg-blue-300/40">
-            <QuestionBox data={QuestionData={count: ques.Answers.length, name: ques.UserName, date: ques.createdAt, question: ques.Question, userId: userID, Tid:ques.UserId , id: ques._id , setUpdate: setUpdate, update: update}} />
-        </div>
-        {ques.Answers.map(ans => (
-          <div className="bg-gray-100 ">
-              <AnswerBox data={AnswerData = {answerPID: ans.answerPersonID, name: ans.answerPersonName, answer: ans.answer, userId: userID, answerID: ans._id, id: ques._id, setUpdate: setUpdate, update: update }} />
+          <div className="bg-blue-300/40">
+              <QuestionBox data={QuestionData={count: ques.Answers.length, name: ques.UserName, date: ques.createdAt, question: ques.Question, userId: userID, Tid:ques.UserId , id: ques._id , setUpdate: setUpdate, update: update}} />
+          </div>
+          {ques.Answers.map(ans => (
+            <div className="bg-gray-100 ">
+                <AnswerBox data={AnswerData = {answerPID: ans.answerPersonID, name: ans.answerPersonName, answer: ans.answer, userId: userID, answerID: ans._id, id: ques._id, setUpdate: setUpdate, update: update }} />
+            </div>
+          ))}
           </div>
         ))}
-        </div>
-      ))}
+      </div>
       {quesModal && (<AddQuestionModal groupData={group} setAddQuestionModal={setQuesModal} updater={updater}/>)}
     </>
   );
