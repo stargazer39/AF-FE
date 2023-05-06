0011b0f2371f3347169ac79d4e6620905e73dd04
Semora <senalweerasekara@gmail.com>
2023-05-07 00:53:21 +0530

    changes

--- src/pages/Auth/Verify.js
+++ src/pages/Auth/Verify.js
@@ -26,9 +26,7 @@ const Login = () => {
       
       <div className='w-1/2 flex justify-center'>
         <div className='flex flex-col justify-center items-center h-screen w-full'>
-          <div className='w-3/5 bg-gray-200 text-center'>
-            <h1 className='mt-4 text-xl text-blue-600'>EduCircle </h1>
-          </div>
+   
           <div className="flex bg-gray-200 justify-center items-center w-3/5 h-screen  p-16">
             <form onSubmit={handleForm} className="flex flex-col items-center">
               <span className="font-medium text-3xl text-gray-900 ">Enter Your <span className='text-blue-700'>Verification</span>  Code</span>
@@ -40,7 +38,7 @@ const Login = () => {
                 placeholder="Verification Code"
               />
               <div className='w-full flex justify-end'>
-                <button className="bg-blue-500 hover:bg-blue-600 w-36 active:bg-blue-700 font-bold mt-10 w-1/4 h-10 rounded-[5px] flex justify-center items-center text-base font-normal">
+                <button className="bg-blue-500 hover:bg-blue-600 w-36 active:bg-blue-700 font-bold mt-8 w-1/4 h-10 rounded-[5px] flex justify-center items-center text-base font-normal">
                   Continue
                 </button>
               </div>
