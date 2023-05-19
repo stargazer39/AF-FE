c02f8a20060acc48752a25e821e2a342c5f439ea
Nisal-Sashmitha <it20081034@my.sliit.lk>
2023-05-20 00:21:23 +0530

    refactor: changed to config const laskshikas part

--- src/services/User.js
+++ src/services/User.js
@@ -1,48 +1,37 @@
 import axios from "axios";
+import { API_ENDPOINT } from "../config";
 
 export const login = async (data) => {
-  const response = await axios.post(
-    "http://localhost:3002/api/auth/login",
-    data
-  );
+  const response = await axios.post(`${API_ENDPOINT}/api/auth/login`, data);
   return response;
 };
 
 export const register = async (data) => {
-  const response = await axios.post(
-    "http://localhost:3002/api/auth/register",
-    data
-  );
+  const response = await axios.post(`${API_ENDPOINT}/api/auth/register`, data);
   return response;
 };
 
 export const verify = async (data) => {
   const response = await axios.post(
-    "http://localhost:3002/api/auth/register/verify",
+    `${API_ENDPOINT}/api/auth/register/verify`,
     data
   );
   return response;
 };
 
 export const updateUser = async (data) => {
-  const response = await axios.post(
-    "http://localhost:3002/api/user/editUser",
-    data
-  );
+  const response = await axios.post(`${API_ENDPOINT}/api/user/editUser`, data);
   return response;
 };
 
 export const getUser = async (data) => {
   const response = await axios.post(
-    "http://localhost:3002/api/user/getOneUser",
+    `${API_ENDPOINT}/api/user/getOneUser`,
     data
   );
   return response;
 };
 export const deleteUser = async (data) => {
-  const response = await axios.post(
-    "http://localhost:3002/api/user/delete",
-    data
-  );
+  const response = await axios.post(`${API_ENDPOINT}/api/user/delete`, data);
   return response;
 };
